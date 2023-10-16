fccaf57d23c5a027ac005b9b95d26d2643a054d5
MahfuzulAlam <a_mahfuzul@yahoo.com>
2023-10-16 20:06:53 +0600

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,105 @@
 
 jQuery(document).ready(function ($) {
   // Write your javascript code here
+  if ($(".directorist-search-contents").length) {
+    $("body").off("change", ".directorist-category-select");
+    $("body").on("change", ".directorist-category-select", function (event) {
+      var $this = $(this);
+      var $container = $this.parents("form");
+      var cat_id = $this.val();
+      var directory_type = $container.find(".listing_type").val();
+      var $search_form_box = $container.find(
+        ".directorist-search-form-box-wrap"
+      );
+      var form_data = new FormData();
+      form_data.append("action", "directorist_category_custom_field_search");
+      form_data.append("nonce", directorist.directorist_nonce);
+      form_data.append("listing_type", directory_type);
+      form_data.append("cat_id", cat_id);
+      form_data.append("atts", JSON.stringify($container.data("atts")));
+      $search_form_box.addClass("atbdp-form-fade");
+      $.ajax({
+        method: "POST",
+        processData: false,
+        contentType: false,
+        url: directorist.ajax_url,
+        data: form_data,
+        success: function success(response) {
+          if (response) {
+            $search_form_box.html(response["search_form"]);
+            $container
+              .find(".directorist-category-select option")
+              .data("custom-field", 1);
+            $container.find(".directorist-category-select").val(cat_id);
+            [
+              new CustomEvent("directorist-search-form-nav-tab-reloaded"),
+              new CustomEvent("directorist-reload-select2-fields"),
+              new CustomEvent("directorist-reload-map-api-field"),
+              new CustomEvent("triggerSlice"),
+            ].forEach(function (event) {
+              document.body.dispatchEvent(event);
+              window.dispatchEvent(event);
+            });
+          }
+
+          $search_form_box.removeClass("atbdp-form-fade");
+        },
+        error: function error(_error) {
+          //console.log(_error);
+        },
+        complete: function () {
+          directorist_get_category_based_tags(cat_id);
+        },
+      });
+    });
+  } // load custom fields of the selected category in the search form
+
+  // Category based tags
+  function directorist_get_category_based_tags(cat_id) {
+    var tag_source = $("#directorist_tag_source").val();
+    if (tag_source !== "all_tags") return;
+
+    var form_data = new FormData();
+    form_data.append("action", "directorist_get_category_based_tags");
+    form_data.append("nonce", directorist.directorist_nonce);
+    form_data.append("cat_id", cat_id);
+
+    $.ajax({
+      method: "POST",
+      processData: false,
+      contentType: false,
+      url: directorist.ajax_url,
+      data: form_data,
+      success: function success(response) {
+        if (response) {
+          if (response.tags_html !== "") {
+            $(".directorist-search-field > .directorist-search-tags").empty();
+            $(".directorist-search-field > .directorist-search-tags").html(
+              response.tags_html
+            );
+          }
+          // $search_form_box.html(response["search_form"]);
+          // $container
+          //   .find(".directorist-category-select option")
+          //   .data("custom-field", 1);
+          // $container.find(".directorist-category-select").val(cat_id);
+          [
+            new CustomEvent("directorist-search-form-nav-tab-reloaded"),
+            new CustomEvent("directorist-reload-select2-fields"),
+            new CustomEvent("directorist-reload-map-api-field"),
+            new CustomEvent("triggerSlice"),
+          ].forEach(function (event) {
+            document.body.dispatchEvent(event);
+            window.dispatchEvent(event);
+          });
+        }
+      },
+      error: function error(_error) {
+        //console.log(_error);
+      },
+      complete: function () {
+        //directorist_get_category_based_tags(cat_id);
+      },
+    });
+  }
 });
--- inc/functions.php
+++ inc/functions.php
@@ -4,4 +4,112 @@
  * Add your custom php code here
  */
 
+add_action( 'wp_ajax_directorist_get_category_based_tags', 'directorist_custom_code_get_category_based_tags' );
+add_action( 'wp_ajax_nopriv_directorist_get_category_based_tags', 'directorist_custom_code_get_category_based_tags' );
 
+function directorist_custom_code_get_category_based_tags() {
+
+    if ( !directorist_verify_nonce( 'nonce' ) ) {
+        wp_send_json(
+            [
+                'search_form' => __( 'Something went wrong, please try again.', 'directorist' ),
+            ]
+        );
+    }
+
+    $tags = [];
+    $tags_html = '';
+
+    $cat_id = !empty( $_POST['cat_id'] ) ? sanitize_key( $_POST['cat_id'] ) : '';
+
+    if ( $cat_id ) {
+        $listings = directorist_custom_code_get_listings_from_category( $cat_id );
+
+        if ( $listings && count( $listings ) > 0 ) {
+            $tags = directorist_custom_code_get_tags_from_listings( $listings );
+        }
+
+    }
+
+    if ( $tags && count( $tags ) > 0 ) {
+        ob_start();
+        directorist_custom_code_prepare_tag_field_html( $tags );
+        $search_form = ob_get_clean();
+        //$tags_html = '<input type="text" name="hello"/>';
+    }
+
+    wp_send_json(
+        [
+            'tags_html' => $search_form,
+        ]
+    );
+}
+
+function directorist_custom_code_get_listings_from_category( $cat_id ) {
+    $args = [
+        'post_type'      => ATBDP_POST_TYPE,
+        'tax_query'      => [
+            [
+                'taxonomy' => 'at_biz_dir-category',
+                'fields'   => 'id',
+                'terms'    => $cat_id, // The term ID you want to query
+            ],
+        ],
+        'posts_per_page' => -1, // Retrieve all matching posts
+        'fields' => 'ids',
+    ];
+
+    $query = new WP_Query( $args );
+
+    if ( $query ) {
+        return $query->posts;
+    } else {
+        return false;
+    }
+
+}
+
+function directorist_custom_code_get_tags_from_listings( $listings ) {
+    $terms_for_posts = [];
+
+    foreach ( $listings as $listing ) {
+        $post_terms = wp_get_post_terms( $listing, ATBDP_TAGS );
+
+        if ( !empty( $post_terms ) ) {
+
+            foreach ( $post_terms as $post_term ) {
+
+                if ( !array_key_exists( $post_term->term_id, $terms_for_posts ) ) {
+                    $terms_for_posts[$post_term->term_id] = $post_term;
+                }
+
+            }
+
+        }
+
+    }
+
+    return $terms_for_posts;
+
+}
+
+function directorist_custom_code_prepare_tag_field_html( $tag_terms ) {
+
+    if ( $tag_terms && count( $tag_terms ) > 0 ) {
+        $rand = rand();
+
+        foreach ( $tag_terms as $term ) {
+            $id = $rand . $term->term_id;
+            ?>
+
+			<div class="directorist-checkbox directorist-checkbox-primary">
+				<input type="checkbox" name="in_tag[]" value="<?php echo esc_attr( $term->term_id ); ?>" id="<?php echo esc_attr( $id ); ?>">
+				<label for="<?php echo esc_attr( $id ); ?>" class="directorist-checkbox__label"><?php echo esc_html( $term->name ); ?></label>
+			</div>
+
+			<?php
+}
+
+    }
+
+}
