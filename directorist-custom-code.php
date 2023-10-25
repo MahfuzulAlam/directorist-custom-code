871e3e0459e67f8884915995b12db776ef749a15
MahfuzulAlam <a_mahfuzul@yahoo.com>
2023-10-25 10:23:24 +0600

    init

--- directorist-custom-code.php
+++ directorist-custom-code.php
@@ -1,6 +1,6 @@
 <?php
 
-/** 
+/**
  * @package  Directorist - Custom Code
  */
 
@@ -23,14 +23,14 @@
 /**
  * If this file is called directly, abrot!!!
  */
-if (!defined('ABSPATH')) {
-    exit;                      // Exit if accessed
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed
 }
 
-if (!class_exists('Directorist_Custom_Code')) {
+if ( !class_exists( 'Directorist_Custom_Code' ) ) {
 
-    final class Directorist_Custom_Code
-    {
+    final class Directorist_Custom_Code {
         /**
          * Instance
          */
@@ -39,20 +39,20 @@ if (!class_exists('Directorist_Custom_Code')) {
         /**
          * Instance
          */
-        public static function instance()
-        {
-            if (!isset(self::$instance) && !(self::$instance instanceof Directorist_Custom_Code)) {
+        public static function instance() {
+
+            if ( !isset( self::$instance ) && !( self::$instance instanceof Directorist_Custom_Code ) ) {
                 self::$instance = new Directorist_Custom_Code;
                 self::$instance->init();
             }
+
             return self::$instance;
         }
 
         /**
          * Init
          */
-        public function init()
-        {
+        public function init() {
             $this->define_constant();
             $this->includes();
             $this->enqueues();
@@ -62,130 +62,144 @@ if (!class_exists('Directorist_Custom_Code')) {
         /**
          * Define
          */
-        public function define_constant()
-        {
-            define('DIRECTORIST_CUSTOM_CODE_URI', plugin_dir_url(__FILE__));
-            define('DIRECTORIST_CUSTOM_CODE_DIR', plugin_dir_path(__FILE__));
+        public function define_constant() {
+
+            if ( !defined( 'DIRECTORIST_CUSTOM_CODE_URI' ) ) {
+                define( 'DIRECTORIST_CUSTOM_CODE_URI', plugin_dir_url( __FILE__ ) );
+            }
+
+            if ( !defined( 'DIRECTORIST_CUSTOM_CODE_DIR' ) ) {
+                define( 'DIRECTORIST_CUSTOM_CODE_DIR', plugin_dir_path( __FILE__ ) );
+            }
+
+            if ( !defined( 'DIRECTORIST_CUSTOM_MAP_VERSION' ) ) {
+                define( 'DIRECTORIST_CUSTOM_MAP_VERSION', '1.0.0' );
+            }
+
         }
 
         /**
          * Included Files
          */
-        public function includes()
-        {
-            include_once(DIRECTORIST_CUSTOM_CODE_DIR . '/inc/functions.php');
+        public function includes() {
+            include_once DIRECTORIST_CUSTOM_CODE_DIR . '/inc/functions.php';
         }
 
         /**
          * Enqueues
          */
-        public function enqueues()
-        {
-            add_action('wp_enqueue_scripts', array($this, 'enqueue_styles'));
-            add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+        public function enqueues() {
+            add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_styles' ] );
+            add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
         }
 
         /**
          * Hooks
          */
-        public function hooks()
-        {
-            add_filter('directorist_template', array($this, 'directorist_template'), 10, 2);
+        public function hooks() {
+            add_filter( 'directorist_template', [ $this, 'directorist_template' ], 10, 2 );
         }
 
         /**
          *  Enqueue JS file
          */
-        public function enqueue_scripts()
-        {
+        public function enqueue_scripts() {
             // Replace 'your-plugin-name' with the actual name of your plugin's folder.
-            wp_enqueue_script('directorist-custom-script', DIRECTORIST_CUSTOM_CODE_URI . 'assets/js/main.js', array('jquery'), '1.0', true);
+            wp_enqueue_script( 'directorist-custom-script', DIRECTORIST_CUSTOM_CODE_URI . 'assets/js/main.js', [ 'jquery' ], '1.0', true );
         }
 
         /**
          *  Enqueue CSS file
          */
-        public function enqueue_styles()
-        {
+        public function enqueue_styles() {
             // Replace 'your-plugin-name' with the actual name of your plugin's folder.
-            wp_enqueue_style('directorist-custom-style', DIRECTORIST_CUSTOM_CODE_URI . 'assets/css/main.css', array(), '1.0');
+            wp_enqueue_style( 'directorist-custom-style', DIRECTORIST_CUSTOM_CODE_URI . 'assets/css/main.css', [], '1.0' );
         }
 
         /**
          * Template Exists
          */
-        public function template_exists($template_file)
-        {
+        public function template_exists( $template_file ) {
             $file = DIRECTORIST_CUSTOM_CODE_DIR . '/templates/' . $template_file . '.php';
 
-            if (file_exists($file)) {
+            if ( file_exists( $file ) ) {
                 return true;
             } else {
                 return false;
             }
+
         }
 
         /**
          * Get Template
          */
-        public function get_template($template_file, $args = array())
-        {
-            if (is_array($args)) {
-                extract($args);
+        public function get_template( $template_file, $args = [] ) {
+
+            if ( is_array( $args ) ) {
+                extract( $args );
             }
+
             $data = $args;
 
-            if (isset($args['form'])) $listing_form = $args['form'];
+            if ( isset( $args['form'] ) ) {
+                $listing_form = $args['form'];
+            }
 
             $file = DIRECTORIST_CUSTOM_CODE_DIR . '/templates/' . $template_file . '.php';
 
-            if ($this->template_exists($template_file)) {
+            if ( $this->template_exists( $template_file ) ) {
                 include $file;
             }
+
         }
 
         /**
          * Directorist Template
          */
-        public function directorist_template($template, $field_data)
-        {
-            if ($this->template_exists($template)) $template = $this->get_template($template, $field_data);
+        public function directorist_template( $template, $field_data ) {
+
+            if ( $this->template_exists( $template ) ) {
+                $template = $this->get_template( $template, $field_data );
+            }
+
             return $template;
         }
+
     }
 
-    if (!function_exists('directorist_is_plugin_active')) {
-        function directorist_is_plugin_active($plugin)
-        {
-            return in_array($plugin, (array) get_option('active_plugins', array()), true) || directorist_is_plugin_active_for_network($plugin);
+    if ( !function_exists( 'directorist_is_plugin_active' ) ) {
+        function directorist_is_plugin_active( $plugin ) {
+            return in_array( $plugin, (array) get_option( 'active_plugins', [] ), true ) || directorist_is_plugin_active_for_network( $plugin );
         }
+
     }
 
-    if (!function_exists('directorist_is_plugin_active_for_network')) {
-        function directorist_is_plugin_active_for_network($plugin)
-        {
-            if (!is_multisite()) {
+    if ( !function_exists( 'directorist_is_plugin_active_for_network' ) ) {
+        function directorist_is_plugin_active_for_network( $plugin ) {
+
+            if ( !is_multisite() ) {
                 return false;
             }
 
-            $plugins = get_site_option('active_sitewide_plugins');
-            if (isset($plugins[$plugin])) {
+            $plugins = get_site_option( 'active_sitewide_plugins' );
+
+            if ( isset( $plugins[$plugin] ) ) {
                 return true;
             }
 
             return false;
         }
+
     }
 
-    function Directorist_Custom_Code()
-    {
+    function Directorist_Custom_Code() {
         return Directorist_Custom_Code::instance();
     }
 
-    if (directorist_is_plugin_active('directorist/directorist-base.php')) {
+    if ( directorist_is_plugin_active( 'directorist/directorist-base.php' ) ) {
         Directorist_Custom_Code(); // get the plugin running
     }
-}
 
+}
 
 ?>
--- inc/functions.php
+++ inc/functions.php
@@ -4,4 +4,29 @@
  * Add your custom php code here
  */
 
+function mpp_bbd_inspect_scripts() {
 
+    if ( !is_admin() ) {
+        wp_dequeue_script( 'directorist-google-map' );
+        wp_deregister_script( 'directorist-google-map' );
+    }
+
+}
+
+add_action( 'wp_print_scripts', 'mpp_bbd_inspect_scripts' );
+
+function mpp_custom_google_map_scripts() {
+    //wp_enqueue_style('custom-css', get_stylesheet_directory_uri() . '/assets/css/custom.css');
+    wp_enqueue_script( 'google-map-api' );
+    wp_enqueue_script( 'directorist-markerclusterer' );
+    wp_enqueue_script( 'bbd-custom-google', DIRECTORIST_CUSTOM_CODE_URI . 'assets/js/google-map.js', [ 'google-map-api' ], DIRECTORIST_CUSTOM_MAP_VERSION, true );
+    wp_localize_script( 'bbd-custom-google', 'directorist_options', bbd_get_option_data() );
+}
+
+add_action( 'wp_enqueue_scripts', 'mpp_custom_google_map_scripts', 0 );
+
+function bbd_get_option_data() {
+    $options = [];
+    $options['script_debugging'] = get_directorist_option( 'script_debugging', DIRECTORIST_LOAD_MIN_FILES, true );
+    return $options;
+}
