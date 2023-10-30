ac25dc0a03b8750e5fc366abbfb6592309c8a34b
MahfuzulAlam <a_mahfuzul@yahoo.com>
2023-10-31 03:56:56 +0600

    add: coupon code badge

--- inc/functions.php
+++ inc/functions.php
@@ -4,9 +4,13 @@
  * Add your custom php code here
  */
 
-add_filter( 'atbdp_listing_type_settings_field_list', 'custom_atbdp_listing_type_settings_field_list' );
+ /**
+  * Free Trial Badge
+  */
 
-function custom_atbdp_listing_type_settings_field_list( $fields ) {
+add_filter( 'atbdp_listing_type_settings_field_list', 'free_trial_custom_atbdp_listing_type_settings_field_list' );
+
+function free_trial_custom_atbdp_listing_type_settings_field_list( $fields ) {
 
     foreach ( $fields as $key => $value ) {
         // setup widgets
@@ -59,9 +63,9 @@ function custom_atbdp_listing_type_settings_field_list( $fields ) {
 }
 
 
-add_action( 'atbdp_all_listings_badge_template', 'custom_atbdp_all_listings_badge_template' );
+add_action( 'atbdp_all_listings_badge_template', 'free_trial_custom_atbdp_all_listings_badge_template' );
 
-function custom_atbdp_all_listings_badge_template( $field ) {
+function free_trial_custom_atbdp_all_listings_badge_template( $field ) {
 
     switch ($field['widget_key']) {
         case 'free_trial_badge':
@@ -133,6 +137,9 @@ function affiliate_custom_atbdp_listing_type_settings_field_list( $fields ) {
     return $fields;
 }
 
+/**
+ * Affiliate Program Badge
+ */
 
 add_action( 'atbdp_all_listings_badge_template', 'affiliate_atbdp_all_listings_badge_template' );
 
@@ -146,6 +153,85 @@ function affiliate_atbdp_all_listings_badge_template( $field ) {
             if ( $affiliate_program == 'yes' ):
             ?>
                 <span class="directorist-badge directorist-info-item directorist-badge-affiliate-program">Affiliate Program</span>
+            <?php
+            endif;
+
+        break;
+    } 
+
+}
+
+/**
+ * Coupon Code Badge
+ */
+
+add_filter( 'atbdp_listing_type_settings_field_list', 'coupon_code_custom_atbdp_listing_type_settings_field_list' );
+
+function coupon_code_custom_atbdp_listing_type_settings_field_list( $fields ) {
+
+    foreach ( $fields as $key => $value ) {
+        // setup widgets
+        $coupon_code_widget = [
+            'type'    => "badge",
+            'id'      => "coupon_code_badge",
+            'label'   => "Coupon Code",
+            'icon'    => "uil uil-text-fields",
+            'hook'    => "atbdp_coupon_code_badge",
+            'options' => [],
+        ];
+
+        if ( 'listings_card_grid_view' === $key ) {
+            // register widget
+            $fields[$key]['card_templates']['grid_view_with_thumbnail']['widgets']['coupon_code_badge'] = $coupon_code_widget;
+            $fields[$key]['card_templates']['grid_view_without_thumbnail']['widgets']['coupon_code_badge'] = $coupon_code_widget;
+
+            // grid with preview image
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['thumbnail']['top_right']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['thumbnail']['top_left']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['thumbnail']['bottom_right']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['thumbnail']['bottom_left']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['body']['top']['acceptedWidgets'], 'coupon_code_badge' );
+
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['footer']['right']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['grid_view_with_thumbnail']['layout']['footer']['left']['acceptedWidgets'], 'coupon_code_badge' );
+
+            // grid without preview image
+            array_push( $fields[$key]['card_templates']['grid_view_without_thumbnail']['layout']['body']['quick_info']['acceptedWidgets'], 'coupon_code_badge' );
+        }
+
+        if ( 'listings_card_list_view' === $key ) {
+            // register widget
+            $fields[$key]['card_templates']['list_view_with_thumbnail']['widgets']['coupon_code_badge'] = $coupon_code_widget;
+            $fields[$key]['card_templates']['list_view_without_thumbnail']['widgets']['coupon_code_badge'] = $coupon_code_widget;
+
+            // grid with preview image
+            array_push( $fields[$key]['card_templates']['list_view_with_thumbnail']['layout']['thumbnail']['top_right']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['list_view_with_thumbnail']['layout']['body']['top']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['list_view_with_thumbnail']['layout']['body']['right']['acceptedWidgets'], 'coupon_code_badge' );
+
+            // grid without preview image
+            array_push( $fields[$key]['card_templates']['list_view_without_thumbnail']['layout']['body']['top']['acceptedWidgets'], 'coupon_code_badge' );
+            array_push( $fields[$key]['card_templates']['list_view_without_thumbnail']['layout']['body']['right']['acceptedWidgets'], 'coupon_code_badge' );
+        }
+
+    }
+
+    return $fields;
+}
+
+
+add_action( 'atbdp_all_listings_badge_template', 'coupon_code_atbdp_all_listings_badge_template' );
+
+function coupon_code_atbdp_all_listings_badge_template( $field ) {
+
+    switch ($field['widget_key']) {
+        case 'coupon_code_badge':
+
+            $coupon_code = get_post_meta(get_the_ID(), '_coupon_code', true);
+
+            if ( $coupon_code && !empty($coupon_code) ):
+            ?>
+                <span class="directorist-badge directorist-info-item directorist-badge-coupon-code">Coupon Code</span>
             <?php
             endif;
 
