bfa343e191382b4f1181ab1dbb6a726cfa40376f
circlebd <a_mahfuzul@yahoo.com>
2024-03-13 12:44:04 +0600

    init

--- inc/functions.php
+++ inc/functions.php
@@ -5,3 +5,7 @@
  */
 
 
+add_action( 'wp_update_user', function( $user_id ){
+    $value	=   isset( $_POST['user']['instagram'] ) && !empty( $_POST['user']['instagram'] ) ? sanitize_url( trim( $_POST['user']['instagram'] ) ) : '';
+    update_user_meta( $user_id, 'atbdp_instagram', $value );
+} );
