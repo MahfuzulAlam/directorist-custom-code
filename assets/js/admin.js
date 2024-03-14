bc2dd7dd37797e5c47d33e437fff20dc4b84de91
MahfuzulAlam <a_mahfuzul@yahoo.com>
2024-03-15 00:19:58 +0600

    fix: datetime issue

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -6,29 +6,32 @@ jQuery(document).ready(function ($) {
   // Write your javascript code here
   $(function() {
 
-      var start = moment().subtract(29, 'days');
-      var end = moment();
+    var start_date = $('input[name=start_date]').val();
+    var end_date = $('input[name=end_date]').val();
 
-      function cb(start, end) {
-          $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-          $('input[name=start_date]').val( start.format('YYYY-MM-DD') );
-          $('input[name=end_date]').val( end.format('YYYY-MM-DD') );
-      }
+    var start = start_date ? moment( start_date ) : moment();
+    var end = end_date ? moment( end_date ) : moment().add( 30, 'days' );
 
-      $('#reportrange').daterangepicker({
-          startDate: start,
-          endDate: end,
-          ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-          }
-      }, cb);
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        $('input[name=start_date]').val( start.format('YYYY-MM-DD') );
+        $('input[name=end_date]').val( end.format('YYYY-MM-DD') );
+    }
 
-      cb(start, end);
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+          'Today': [moment(), moment()],
+          'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month': [moment().startOf('month'), moment().endOf('month')],
+          'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
 
   });
 });
--- templates/admin/submenu-statistics.php
+++ templates/admin/submenu-statistics.php
@@ -12,8 +12,8 @@
         <form method='post' action='<?php echo $_SERVER['REQUEST_URI']; ?>' enctype='multipart/form-data'>
             <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
                 <span></span> <i class="fa fa-caret-down"></i>
-                <input type="hidden" name="start_date" />
-                <input type="hidden" name="end_date" />
+                <input type="hidden" name="start_date" value="<?php echo isset( $_POST[ 'start_date' ] ) && !empty( $_POST[ 'start_date' ] ) ? $_POST[ 'start_date' ] : ''; ?>"/>
+                <input type="hidden" name="end_date" value="<?php echo isset( $_POST[ 'end_date' ] ) && !empty( $_POST[ 'end_date' ] ) ? $_POST[ 'end_date' ] : ''; ?>"/>
             </div>
             <input type="submit" name="stat_date_time_submit" value="Search" class="button button-primary">
         </form>
