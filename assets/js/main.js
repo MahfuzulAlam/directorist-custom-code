bfeeb37632d53e4c9d68c13f840692ef5d4339db
Mahfuz <a_mahfuzul@yahoo.com>
2025-02-15 17:44:51 +0600

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,63 @@
 jQuery(document).ready(function ($) {
   // Write your javascript code here
 });
+/**
+ *   Add your custom JS here
+ * */
+
+jQuery(document).ready(function ($) {
+  // Write your javascript code here
+  directorist_add_listing_page_custom_code();
+
+  // add listing page custom code
+  function directorist_add_listing_page_custom_code()
+  {
+    $('#directorist-add-listing-form #at_biz_dir-categories_parent').select2();
+    $('#directorist-add-listing-form #at_biz_dir-categories_child').select2();
+
+    $('#directorist-add-listing-form select[name="admin_category_select_parent"]').on("change", function () {
+      var parent_value = $(this).val();
+
+      var category_children = $("#category_children").val();
+
+      if (category_children) {
+        category_children = JSON.parse(category_children);
+        category_children = category_children[parent_value];
+      }
+
+      // Get a reference to the Select2 element
+      var catChild = $('select[name="admin_category_select_child[]"]');
+
+      // Clear existing options from the Select2 element
+      catChild.empty();
+
+      // Create new options
+      var newOptions = [];
+      if (category_children) {
+        category_children.forEach((child) => {
+          newOptions.push({
+            id: child.term_id,
+            text: child.name,
+          });
+        });
+        $('.directorist-form-sub-categories-field').css({visibility: 'visible', height: 'auto'});
+      }else{
+        $('.directorist-form-sub-categories-field').css({visibility: 'hidden', height: '0'});
+      }
+
+      // Add the new options to the Select2 element
+      catChild.select2({
+        data: newOptions,
+      });
+      catChild.val(null).trigger('change');
+      // Set parent value
+      $('#at_biz_dir-categories').val(parent_value).trigger('change');
+    });
+
+    $('#directorist-add-listing-form #at_biz_dir-categories_child').on("change", function (e) {
+      e.preventDefault();
+      var child_value = $(this).val();
+      $('#at_biz_dir-categories').val(child_value).trigger('change');
+    });
+  }
+});
--- inc/functions.php
+++ inc/functions.php
@@ -4,4 +4,21 @@
  * Add your custom php code here
  */
 
-
+ add_action('atbdp_listing_expired', function($listing_id) {
+	// Change pricing plan
+	$free_plan_id = 45; // Please use the free plan ID here
+	update_post_meta($listing_id, '_fm_plans', $free_plan_id);
+	update_post_meta($listing_id, '_fm_plans_by_admin', 1);
+	
+	// Set the listing to never expire
+	update_post_meta($listing_id, '_never_expire', 1);
+	
+	// Publish the listing
+	wp_update_post(array(
+		'ID' => $listing_id,
+		'post_status' => 'publish',
+		'meta_input' => [
+		'_listing_status' => 'post_status',
+		],
+	));
+});
