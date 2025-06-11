1a6ee1edb5ccfcec82d151f5c1af81d1fa307120
Mahfuz <a_mahfuzul@yahoo.com>
2025-06-11 16:56:57 +0600

    init

--- inc/functions.php
+++ inc/functions.php
@@ -5,3 +5,7 @@
  */
 
 
+add_action( 'atbdp_user_registration_completed', function ( $user_id ){
+    $nid	=   isset( $_POST['nid'] ) && !empty( $_POST['nid'] ) ? sanitize_text_field( trim( $_POST['nid'] ) ) : '';
+    if( $nid ) update_user_meta( $user_id, '_nid', $nid );
+} );
