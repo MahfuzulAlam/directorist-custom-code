cdfec1e48af02b017418f5563eb4bce3af8c1a77
Mahfuz <a_mahfuzul@yahoo.com>
2025-09-25 17:15:15 +0600

    init

--- inc/functions.php
+++ inc/functions.php
@@ -4,4 +4,23 @@
  * Add your custom php code here
  */
 
+add_filter('atbdp_single_listing_content_widgets', function($widgets){
+    $widgets['excerpt'] = [
+        'options' => [
+            'icon' => [
+                'type'  => 'icon',
+                'label' => 'Icon',
+                'value' => 'las la-code',
+            ],
+        ]
+    ];
+    return $widgets;
+});
 
+
+add_filter( 'directorist_single_item_template', function($template, $field_data){
+    if ( isset($field_data['widget_name']) && 'excerpt' === $field_data['widget_name'] && !empty($field_data['value']) ) {
+        echo '<div class="directorist-single-info directorist-listing-details__text">'.$field_data['value'].'</div>';
+    }
+    return $template;
+}, 10, 2 );
