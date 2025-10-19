9a112f39acfa5c80c624bda9f24b187853ccbb7d
Mahfuz <a_mahfuzul@yahoo.com>
2025-10-19 23:54:10 +0600

    update css

--- inc/functions.php
+++ inc/functions.php
@@ -5,7 +5,7 @@
  */
 
 
- add_action( 'init', function(){
+add_action( 'init', function(){
     new Directorist_Custom_Registration_Field('Company Name', 'company_name', 'text');
     new Directorist_Custom_Registration_Field('Vat Number', 'vat_number', 'text');
 } );
